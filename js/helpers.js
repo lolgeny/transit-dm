b9d47b8b69d9c1c4d4d0efe10965d1e272757b98
Joel Evgeny Swedensky <Joel@BigMac.local>
2018-04-11 18:15:35 +0100

    --

--- js/helpers.js
+++ js/helpers.js
@@ -1,9 +1,12 @@
 function match_heights() {
     var id_list = arguments;
     window.onload = function() {
+        for (id in id_list) {
+          $('#' + id_list[id]).css('height', 'auto');
+        }
         var max_height = 0;
         for (id in id_list) {
-            max_height = Math.max(max_height, $('#' + id_list[id]).innerHeight());
+            max_height = Math.max(max_height, $('#' + id_list[id]).outerHeight());
         };
         for (id in id_list) {
             $('#' + id_list[id]).innerHeight(max_height);
